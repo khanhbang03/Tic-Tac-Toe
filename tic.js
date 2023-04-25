3198ab518366a6c07c99cdfb8d069212f9d532c4
Bang Nguyen <46435787+khanhbang03@users.noreply.github.com>
2023-04-26 01:35:47 +1000

    Update tic.js

--- tic.js
+++ tic.js
@@ -42,8 +42,7 @@ function Run5() {
 
 box = document.getElementsByTagName("input");
 
-// In reality, the player will not necessarily play optimally
-// In this program, the computer ends the game whenever it has detected a possible win for the current player (it will try to win whenever it can)
+// The computer tries to win whenever possible
 function win(n, p) {
     if (n == 3) {
         // check for 3 in a row
